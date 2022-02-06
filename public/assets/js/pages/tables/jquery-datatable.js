1044e64a02ad8d4e11e84967c7dc2cf505dcb905
Fozimat <fozimata@gmail.com>
2022-02-06 12:52:46 +0700

    nowrap keterangan datatables

--- public/assets/js/pages/tables/jquery-datatable.js
+++ public/assets/js/pages/tables/jquery-datatable.js
@@ -14,4 +14,36 @@ $(function () {
             { "width": "5%", "targets": 0 }
         ]
     });
+
+    $('.tugas-nowrap').DataTable({
+        dom: 'Bfrtip',
+        responsive: true,
+        buttons: [
+            { extend: 'pdf', className: 'btn bg-purple waves-effect' },
+        ],
+        columnDefs: [{
+            targets: [6],
+            render: function (data, type, row) {
+                return type === 'display' && data.length > 10 ?
+                    data.substr(0, 10) + '…' :
+                    data;
+            }
+        }]
+    });
+
+    $('.materi-nowrap').DataTable({
+        dom: 'Bfrtip',
+        responsive: true,
+        buttons: [
+            { extend: 'pdf', className: 'btn bg-purple waves-effect' },
+        ],
+        columnDefs: [{
+            targets: [5],
+            render: function (data, type, row) {
+                return type === 'display' && data.length > 10 ?
+                    data.substr(0, 10) + '…' :
+                    data;
+            }
+        }]
+    });
 });
