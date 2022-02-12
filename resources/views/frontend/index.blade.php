515b6e2831ed4704b70c15c2b80fcad51bfc8db9
Fozimat <fozimata@gmail.com>
2022-02-12 21:22:35 +0700

    Update why choose us

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -41,7 +41,6 @@
             <nav id=" navbar" class="navbar order-last order-lg-0">
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
@@ -65,44 +64,7 @@
     </section>
 
     <main id="main">
-        <div class="breadcrumbs" data-aos="fade-in">
-            <div class="container">
-                <h2>About Us</h2>
-            </div>
-        </div>
 
-        <section id="about" class="about">
-            <div class="container" data-aos="fade-up">
-                <div class="row">
-                    <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-                        <img src="{{ asset('assets-frontend/img/about.jpg') }}" class="img-fluid" alt="">
-                    </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-                        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-                        <p class="fst-italic">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore
-                            magna aliqua.
-                        </p>
-                        <ul>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat.</li>
-                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate
-                                velit.</li>
-                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda
-                                mastiro dolore eu fugiat nulla pariatur.</li>
-                        </ul>
-                        <p>
-                            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate
-                        </p>
-
-                    </div>
-                </div>
-
-            </div>
-        </section>
 
         <section id="counts" class="counts section-bg">
             <div class="container">
@@ -135,7 +97,9 @@
 
                 </div> --}}
                 <div class="col-lg-12">
-                    <h2 class="text-center font-weight-bold">More info about us! Check on our instagram</h2>
+                    <h2 class="text-center font-weight-bold">More info about us! Check on our <a
+                            href="https://www.instagram.com/kusuma.ilm/" target="_blank">Instagram</a>
+                    </h2>
                 </div>
 
             </div>
@@ -147,15 +111,16 @@
                 <div class="row">
                     <div class="col-lg-4 d-flex align-items-stretch">
                         <div class="content">
-                            <h3>Why Choose Mentor?</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus
-                                optio ad corporis.
+                            <h3>Kenapa harus KUSUMA ?</h3>
+                            <p style="text-align: justify;word-spacing: -2px;">
+                                Para pelajar dapat belajar tanpa rasa bosan karena kami menyediakan mentor yang menarik
+                                dan berpengalaman, serta kami menyediakan fasilitas yang nyaman untuk ruang lingkup
+                                belajar dengan harga yang terjangkau.
+                                Mari, bergabunglah bersama kami di KUSUMA.
                             </p>
                             <div class="text-center">
-                                <a href="about.html" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
+                                <a href="https://www.instagram.com/kusuma.ilm/" target="_blank" class="more-btn">Learn
+                                    More <i class="bx bx-chevron-right"></i></a>
                             </div>
                         </div>
                     </div>
@@ -164,26 +129,25 @@
                             <div class="row">
                                 <div class="col-xl-4 d-flex align-items-stretch">
                                     <div class="icon-box mt-4 mt-xl-0">
-                                        <i class="bx bx-receipt"></i>
-                                        <h4>Corporis voluptates sit</h4>
-                                        <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut
-                                            aliquip</p>
+                                        <i class='bx bxs-school'></i>
+
+                                        <p>KUSUMA merupakan pusat bimbel terbaik diseluruh tanjungpinang
+                                            dengan
+                                            pengajaran belajar yang unik dan menarik</p>
                                     </div>
                                 </div>
                                 <div class="col-xl-4 d-flex align-items-stretch">
                                     <div class="icon-box mt-4 mt-xl-0">
-                                        <i class="bx bx-cube-alt"></i>
-                                        <h4>Ullamco laboris ladore pan</h4>
-                                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                            deserunt</p>
+                                        <i class='bx bxs-calendar'></i>
+                                        <p>Kami menyediakan banyak kelas perharinya dengan "hari belajar" mulai dari
+                                            hari senin - sabtu</p>
                                     </div>
                                 </div>
                                 <div class="col-xl-4 d-flex align-items-stretch">
                                     <div class="icon-box mt-4 mt-xl-0">
-                                        <i class="bx bx-images"></i>
-                                        <h4>Labore consequatur</h4>
-                                        <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis
-                                            facere</p>
+                                        <i class='bx bxs-time'></i>
+                                        <p>Jam operasional kami mulai dari jam 16.00 WIB - 20.30 WIB setiap "hari
+                                            belajar"</p>
                                     </div>
                                 </div>
                             </div>
@@ -255,8 +219,10 @@
 
             </div>
             <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="https://www.youtube.com/channel/UC4Bm_gqCne6OwKoleh42hCA" target="_blank" class="youtube"><i
+                        class="bx bxl-youtube"></i></a>
+                <a href="https://www.instagram.com/kusuma.ilm/" target="_blank" class="instagram"><i
+                        class="bx bxl-instagram"></i></a>
             </div>
         </div>
     </footer>
